14d9472111e2b9bf71ac004de3b85a03c9f3c8ad
ricardobsn <ricardobsn@gmail.com>
2021-03-28 19:52:18 -0300

    apontamento url backend local

--- src/axios.js
+++ src/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 const instance = axios.create({
     // Api Cloud Function url
-    baseURL: "https://us-central1-impar-challenge.cloudfunctions.net/api",
-    // baseURL: "http://localhost:5001/impar-challenge/us-central1/api",
+    // baseURL: "https://us-central1-impar-challenge.cloudfunctions.net/api",
+    baseURL: "http://localhost:5001/impar-challenge/us-central1/api",
 });
 
 export default instance;
